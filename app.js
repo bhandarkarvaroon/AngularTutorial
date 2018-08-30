c23d788fa7e3e597afbac76dc5f465b67cc22bd7
bhandarkarvaroon <bhandarkarvaroon@gmail.com>
2018-08-30 16:34:42 +0530

    adding custom directive in app

--- app.js
+++ app.js
@@ -69,4 +69,11 @@ function($scope,$log,$routeParams,nameService){
 }]);
 
 
+myApp.directive("searchResult",function(){
+    return{
+        template : '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Varun Bhandarkar</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p> </a>'
+    }
+});
+
+
 
